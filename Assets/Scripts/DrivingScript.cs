d6f951775c81e643a4f8f3debfca60ce5cfb1340
Igor Józefowicz <igorjozefowicz@gmail.com>
2025-05-31 01:31:52 +0200

    add special effect - koń mechaniczny

--- Assets/Scripts/DrivingScript.cs
+++ Assets/Scripts/DrivingScript.cs
@@ -41,7 +41,7 @@ public class DrivingScript : MonoBehaviour
             wheel.wheelCollider.GetWorldPose(out position, out quat);
 
             //wheel.wheel.transform.position = position;
-            //wheel.wheel.transform.rotation = quat;
+            wheel.wheel.transform.rotation = quat;
         }
     }
 
