d49040aa52e7dabe901b0eb15935ec4514935143
Igor Józefowicz <igorjozefowicz@gmail.com>
2025-05-31 02:18:43 +0200

    add full lesson 1 & 2 car model

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    DrivingScript drivingScript;
+    DrivingScript driveScript;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        drivingScript = GetComponent<DrivingScript>();
+        driveScript = GetComponent<DrivingScript>();
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        float accelerator = Input.GetAxis("Vertical");
+        float accel = Input.GetAxis("Vertical");
         float steer = Input.GetAxis("Horizontal");
         float brake = Input.GetAxis("Jump");
-
-        drivingScript.Drive(accelerator, brake, steer);
+        driveScript.Drive(accel, brake, steer);
+        ////
+        driveScript.EngineSound();
     }
 }
