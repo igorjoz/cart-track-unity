6a7528ba60d323edd67e79f21581689417707a16
Igor Józefowicz <igorjozefowicz@gmail.com>
2025-06-21 10:00:41 +0200

    add lesson 5 progress

---
+++ Assets/CarAppearance.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using TMPro;
+
+public class CarAppearance : MonoBehaviour
+{
+    public string playerName;
+    public Color carColor;
+    public TMP_Text nameText;
+    public Renderer carRenderer;
+
+    void Start()
+    {
+        nameText.text = playerName;
+        carRenderer.material.color = carColor;
+        nameText.color = carColor;
+    }
+}
--- Assets/Scripts/RaceController.cs
+++ Assets/Scripts/RaceController.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel;
+using TMPro;
 using UnityEngine;
 
 public class RaceController : MonoBehaviour
@@ -8,23 +9,40 @@ public class RaceController : MonoBehaviour
     public static bool isRacing = false;
     public CheckpointController[] carsController;
 
+    public TMP_Text startText;
+    AudioSource audioSource;
+    public AudioClip count;
+    public AudioClip start;
+
     void CountDown()
     {
+        startText.gameObject.SetActive(true);
+
         if (timer < 0)
         {
-            Debug.Log("Rozpoczêcie wyœcigu za: " + timer);
+            //Debug.Log("Rozpoczêcie wyœcigu za: " + timer);
+            startText.text = timer.ToString();
+            audioSource.PlayOneShot(count);
+
             timer--;
         }
         else
         {
-            Debug.Log("Start!");
+            //Debug.Log("Start!");
+            startText.text = "START!!!";
+            audioSource.PlayOneShot(start);
+
             isRacing = true;
             CancelInvoke("CountDown");
+            Invoke("HideStartText", 1);
         }
     }
 
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
+        startText.gameObject.SetActive(false);
+
         InvokeRepeating("CountDown", 3, 1);
 
         GameObject[] cars = GameObject.FindGameObjectsWithTag("Car");
@@ -33,6 +51,10 @@ public class RaceController : MonoBehaviour
         {
             carsController[i] = cars[i].GetComponent<CheckpointController>();
         }
+    }
 
+    void HideStartText()
+    {
+        startText.gameObject.SetActive(false);
     }
 }
